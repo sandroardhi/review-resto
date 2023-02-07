98f535c5d5bd60a1e86006ddec4ec40af42466fd
sandroardhi <sandrosaputra557@gmail.com>
2023-02-07 08:51:18 +0700

    feat: login-bug

--- src/composables/useHttp.js
+++ src/composables/useHttp.js
@@ -3,25 +3,25 @@
 import axios from "axios";
 
 export const useHttp = () => {
-    const client = axios.create({
-        baseURL: import.meta.env.VUE_APP_API_URL || 'http://localhost',
-        headers: {
-            "Content-Type": "application/json",
-            "X-requested-with": "XMLHttpRequest",
-        },
-    });
+  const client = axios.create({
+    baseURL: import.meta.env.VUE_APP_API_URL || "http://localhost:8000",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+    },
+  });
 
-    const get = (url, params) => client.get(url, {params}); 
-    const post = (url, params, data) => client.post(url, data, {params}); 
-    const put = (url, data, params) => client.put(url, data, {params}); 
-    const patch = (url, data, params) => client.patch(url, data, {params}); 
-    const destroy = (url, params) => client.delete(url, {params}); 
+  const get = (url, params) => client.get(url, { params });
+  const post = (url, data, params) => client.post(url, data, { params });
+  const put = (url, data, params) => client.put(url, data, { params });
+  const patch = (url, data, params) => client.patch(url, data, { params });
+  const destroy = (url, params) => client.delete(url, { params });
 
-    return {
-        get,
-        post,
-        put,
-        patch,
-        destroy
-    }
-}
+  return {
+    get,
+    post,
+    put,
+    patch,
+    destroy,
+  };
+};
