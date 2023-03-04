0a19f94281ebddf12632a0340a78913a9f94765d
sandroardhi <sandrosaputra557@gmail.com>
2023-03-04 09:20:28 +0700

    feta: logout

--- src/views/RestoView.vue
+++ src/views/RestoView.vue
@@ -1,7 +1,7 @@
 <script setup>
     import { useRestoRepository } from '../composables/useRestoRepository';
     import { ref, onMounted } from 'vue';
-    import { RouterLink } from 'vue-router';
+    import { RouterLink, useRouter } from 'vue-router';
     import BaseCard from '../components/BaseCard.vue';
     import BaseContainer from '../components/BaseContainer.vue';
 
@@ -9,6 +9,7 @@
 
     const isLoading = ref(true);
     const restos = ref([]);
+    const router = useRouter();
     const fetchRestos = async () => {
         isLoading.value = true;
         try {
@@ -29,18 +30,30 @@
         }
         return textCopy;
     }
+    const logout = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('user');
+    router.replace({name: 'login'})
+    }
 </script>
 
 <template>
-    <BaseContainer>
-        <div class="relative flex justify-between w-full bg-white p-4 mb-8">
+    <div v-if="isLoading" class="w-screen h-screen bg-gray-100 flex items-center justify-center flex-col ">
+        <img src="../../public/assets/loading-cat-unscreen.gif" alt="">
+        <p class=" text-4xl text-[#393d47] font-['Fredoka_One']">Please Wait Congok . . .</p>
+    </div>
+    <BaseContainer v-else>
+        <div class="relative flex justify-between w-full bg-white p-4 mb-8 items-center">
             <div>
-                <h1>Resto</h1>
+                <h1 class="text-3xl font-semibold">Resto</h1>
             </div>
-            <div class="flex justify-around">
-                <a href="" class="ml-2">Home</a>
-                <a href="" class="ml-2">About</a>
-                <router-link to="create" class="ml-2">Create Your Resto</router-link>
+            <div class="flex justify-around items-center">
+                <a href="" class="ml-2 px-4">Home</a>
+                <a href="" class="ml-2 px-4">About</a>
+                <router-link to="create-resto" class="ml-2 px-4">Create Your Resto</router-link>
+                <button @click="logout" class="bg-blue-400 px-2 py-1 text-white font-semibold rounded-md">
+                     Logout
+                </button>
             </div>
         </div>
         <div class="grid grid-cols-12 gap-4">
@@ -52,10 +65,14 @@
                     </template>
                     <div v-if="resto.description">
                         {{ excerpt(resto.description, 40) }}
-                    </div>
+                    </div> 
                     <div v-else>
                         {{ excerpt('No Description Yet', 40) }}
                     </div>
+                    <div v-if="resto.address">
+                        {{ excerpt(resto.address, 40) }}
+                    </div>
+                   
                 </BaseCard>
             </div>
         </div>
