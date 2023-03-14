ce3005812ee0fb8d415bb0f6a042d7cc4323ba03
sandroardhi <sandrosaputra557@gmail.com>
2023-03-14 10:23:52 +0700

    refctor(navbar): making a seperate navbar componnt

---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<script setup>
+    import { ref } from 'vue';
+    import { RouterLink, useRouter } from 'vue-router';
+
+    const userData = JSON.parse(localStorage.getItem('user'));
+    const dropdown = ref(false)
+
+    const dropdownShow = () => {
+        dropdown.value = true
+    }
+    const dropdownHide = () => {
+        dropdown.value = false
+    }
+</script>
+
+
+
+
+<template>    
+        <div class="relative flex justify-between w-full bg-white p-4 mb-8 items-center">
+            <div>
+                <h1 class="text-3xl font-semibold">Resto</h1>
+            </div>
+            <div class="flex justify-around items-center">
+                <a href="" class="ml-2 px-4">Home</a>
+                <router-link to="about" class="ml-2 px-4">About</router-link>
+                <div v-if="userData">
+                    <button v-if="dropdown" @click="dropdownHide" class="dropdown-toggle bg-blue-400 px-2 py-1 text-white font-semibold rounded-md">
+                        Hi, {{ userData.name }}
+                        <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-sm w-[15%] bg-white text-black border">
+                            <li class="p-1 border border-black">
+                                <router-link to="profile">Profile</router-link>
+                            </li>
+                            <li @click="logout" class="p-1  border-x border-black font-semibold">
+                                Logout
+                            </li>
+                            <li class="p-1 border border-black">
+                                <router-link to="create-resto" >Create Your Resto</router-link>
+                            </li>
+                        </ul>
+                    </button>
+                    <button v-else @click="dropdownShow" class="dropdown-toggle bg-blue-400 px-2 py-1 text-white font-semibold rounded-md">
+                        Hi, {{ userData.name }}
+                        <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-sm w-[15%] bg-white text-black border">
+                            <li class="p-1 border border-black">
+                                <router-link to="profile">Profile</router-link> 
+                            </li>
+                            <li @click="logout" class="p-1  border-x border-black font-semibold">
+                                Logout
+                            </li>
+                            <li class="p-1 border border-black">
+                                <router-link to="create-resto" >Create Your Resto</router-link>
+                            </li>
+                        </ul>
+                    </button>
+                </div>
+                <div v-else>
+                    <router-link to="/" >Login</router-link>
+                </div>
+            </div>
+        </div>
+
+</template>
--- src/views/RestoView.vue
+++ src/views/RestoView.vue
@@ -5,6 +5,7 @@
     import { RouterLink, useRouter } from 'vue-router';
     import BaseCard from '../components/BaseCard.vue';
     import BaseContainer from '../components/BaseContainer.vue';
+    import Navbar from '../components/Navbar.vue';
 
     const repository = useRestoRepository();
     const repository_auth = useAuthRepository();
@@ -13,7 +14,6 @@
     const restos = ref([]);
     const router = useRouter();
     
-    const userData = JSON.parse(localStorage.getItem('user'));
 
     const fetchRestos = async () => {
         isLoading.value = true;
@@ -42,14 +42,7 @@
         router.replace({name: 'login'})
     }
 
-    const dropdown = ref(false)
 
-    const dropdownShow = () => {
-        dropdown.value = true
-    }
-    const dropdownHide = () => {
-        dropdown.value = false
-    }
 
     onMounted(() => {
         fetchRestos()
@@ -63,48 +56,7 @@
         <p class=" text-4xl text-[#393d47] font-['Fredoka_One']">Please Wait . . .</p>
     </div>
     <BaseContainer v-else>
-        <div class="relative flex justify-between w-full bg-white p-4 mb-8 items-center">
-            <div>
-                <h1 class="text-3xl font-semibold">Resto</h1>
-            </div>
-            <div class="flex justify-around items-center">
-                <a href="" class="ml-2 px-4">Home</a>
-                <router-link to="about" class="ml-2 px-4">About</router-link>
-                <div v-if="userData">
-                    <button v-if="dropdown" @click="dropdownHide" class="dropdown-toggle bg-blue-400 px-2 py-1 text-white font-semibold rounded-md">
-                        Hi, {{ userData.name }}
-                        <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-sm w-[15%] bg-white text-black border">
-                            <li class="p-1 border border-black">
-                                tes
-                            </li>
-                            <li @click="logout" class="p-1  border-x border-black font-semibold">
-                                Logout
-                            </li>
-                            <li class="p-1 border border-black">
-                                <router-link to="create-resto" >Create Your Resto</router-link>
-                            </li>
-                        </ul>
-                    </button>
-                    <button v-else @click="dropdownShow" class="dropdown-toggle bg-blue-400 px-2 py-1 text-white font-semibold rounded-md">
-                        Hi, {{ userData.name }}
-                        <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-sm w-[15%] bg-white text-black border">
-                            <li class="p-1 border border-black">
-                                tes
-                            </li>
-                            <li @click="logout" class="p-1  border-x border-black font-semibold">
-                                Logout
-                            </li>
-                            <li class="p-1 border border-black">
-                                <router-link to="create-resto" >Create Your Resto</router-link>
-                            </li>
-                        </ul>
-                    </button>
-                </div>
-                <div v-else>
-                    <router-link to="/" >Login</router-link>
-                </div>
-            </div>
-        </div>
+        <Navbar/>
         <div class="grid grid-cols-12 gap-4">
             <div v-for="resto in restos" :key="resto.id" class="col-span-4">
                 <!-- Card -->
